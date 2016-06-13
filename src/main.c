f278cb680b5edb2ac14dcf4c43a67914d7a16e33
Quistnix <fransvb@gmail.com>
2016-06-13 23:54:43 +0200

    Test

--- src/main.c
+++ src/main.c
@@ -7,9 +7,34 @@ static BitmapLayer *s_background_layer;
 static GBitmap *s_background_bitmap;
 static Layer *s_canvas;
 static Layer *s_canvas_line;
+static Layer *s_arrows;
+static const GPathInfo ARROW_LEFT_PATH_POINTS = {
+	3,
+	   (GPoint []) {{0, 0}, {0, 14}, {8, 7},}
+	};
+static const GPathInfo ARROW_RIGHT_PATH_POINTS = {
+	3,
+	   (GPoint []) {{8, 0}, {8, 14}, {0, 7},}
+	};
 static int firstrun = 1;
-#define COLORBG GColorBlack;
-#define COLORFR GColorWhite;
+
+
+
+// Customizations
+#define COLORBG GColorBlack
+//#define COLORHRFR GColorWhite
+//#define COLORHRBG GColorClear
+#define COLORHRBG GColorWhite
+#define COLORHRFR GColorBlack
+#define COLORMNBG GColorClear
+#define COLORMNFR GColorWhite
+#define COLORTRICL GColorRed
+#define COLORTRIBW GColorWhite
+static int twelveHour = 1;
+static int xOffset = 15;
+static int pmSpace = 0;
+
+
 
 // Draw border to hide shader noise
 static void canvas_update_proc(Layer *layer, GContext *ctx) {
@@ -105,7 +130,10 @@ void layer_update_proc(Layer *layer, GContext *ctx) {
 	GRect bounds = layer_get_bounds(layer);
 
 	//set variables for calculations
-		int rowHalf;
+		int rowHalf = bounds.size.w/2;
+		int rowFull = bounds.size.w;
+		int colHalf = bounds.size.h/2;
+		int colFull = bounds.size.h;
 		int xToUse;
 		int xToGet;
 		int yToUse;
@@ -118,7 +146,6 @@ void layer_update_proc(Layer *layer, GContext *ctx) {
 	for(int y = 0; y < bounds.size.h; y++) {
 	  	// Get this row's range and data
 	  	GBitmapDataRowInfo info = gbitmap_get_data_row_info(fb, y);
-		rowHalf = (info.min_x + info.max_x) / 2 ;	
 
 		if (y < (bounds.size.h/2)) {
 			// top half
@@ -131,23 +158,19 @@ void layer_update_proc(Layer *layer, GContext *ctx) {
 			yToSet = y;
 			yToGet = yToSet + (64/(yToUse));
 		} 
-		
-		
+
 	  // Iterate over all visible columns
 		  for(int x = 0; x <= info.max_x; x++) {
 			  // Split in left and right halves
-			  if (x < rowHalf + 0) {
+			  if (x < rowHalf) {
 				  // left half: Work from right to left
-				  xToUse = rowHalf + 0 - x;
-				  xToGet = xToUse - ((x*3)/(yToUse));
-
+				  xToUse = rowHalf - x;
+				  xToGet = xToUse - (x*3)/(yToUse);
 			  } else {
 				  // right half: Work from left to right
 				  xToUse = x;
-				  xToGet = x + (((xToUse - rowHalf + 0)*3)/(yToUse));
+				  xToGet = x + ((xToUse - rowHalf + 0)*3)/(yToUse);
 			  }
-			  
-			  
 			  // is the target pixel inside the area?
 			  if (xToGet < 0 || xToGet > info.max_x || yToGet < 0 || yToGet > bounds.size.h ){
 				  // No, so we'll use the background color
@@ -164,7 +187,25 @@ void layer_update_proc(Layer *layer, GContext *ctx) {
   graphics_release_frame_buffer(ctx, fb);
 }
 
+// layer for arrows
+void s_arrows_update_proc(Layer *s_arrows, GContext* ctx) {
+	static GPath *s_arrow_left_path = NULL;
+	static GPath *s_arrow_right_path = NULL;
 
+	// Fill the path:
+	#if defined(PBL_COLOR)
+  	graphics_context_set_fill_color(ctx, COLORTRICL);
+	#elif defined(PBL_BW)
+  	graphics_context_set_fill_color(ctx, COLORTRIBW);
+	#endif
+	
+	gpath_draw_filled(ctx, s_arrow_left_path);
+	gpath_draw_filled(ctx, s_arrow_right_path);
+
+	s_arrow_left_path = gpath_create(&ARROW_LEFT_PATH_POINTS);
+	s_arrow_right_path = gpath_create(&ARROW_RIGHT_PATH_POINTS);
+	gpath_move_to(s_arrow_right_path, GPoint(102+(pmSpace*2), 0));
+}
 
 static void update_time() {
   // Get a tm structure
@@ -198,15 +239,15 @@ Layer *root_layer = window_get_root_layer(s_main_window);
 
 	GRect start_h;
 	if(s_minute == 00 || firstrun == 1) {
-	  	start_h = GRect((bounds.size.w/2)-47+10,(bounds.size.h/2)-132-((s_hour-1)*36), 47, 1400);
+	  	start_h = GRect((bounds.size.w/2)-49+xOffset,(bounds.size.h/2)-132-((s_hour-1)*36), 47, 1400);
 		firstrun = 0;
 	} else {
-		start_h = GRect((bounds.size.w/2)-47+10,(bounds.size.h/2)-132-(s_hour*36), 47, 1400);
+		start_h = GRect((bounds.size.w/2)-49+xOffset,(bounds.size.h/2)-132-(s_hour*36), 47, 1400);
 	}
-	GRect finish_h = GRect((bounds.size.w/2)-47+10,(bounds.size.h/2)-132-(s_hour*36), 47, 1400);
+	GRect finish_h = GRect((bounds.size.w/2)-49+xOffset,(bounds.size.h/2)-132-(s_hour*36), 47, 1400);
 	
-	GRect start_m = GRect((bounds.size.w/2)+10,(bounds.size.h/2)-114-((s_minute-1)*20), 27, 1888);
-	GRect finish_m = GRect((bounds.size.w/2)+10,(bounds.size.h/2)-114-(s_minute*20), 27, 1888);
+	GRect start_m = GRect((bounds.size.w/2)+xOffset,(bounds.size.h/2)-114-((s_minute-1)*20), 27, 1888);
+	GRect finish_m = GRect((bounds.size.w/2)+xOffset,(bounds.size.h/2)-114-(s_minute*20), 27, 1888);
 	
 
 	// Animate the Layer
@@ -255,19 +296,23 @@ static void main_window_load(Window *window) {
   GRect bounds = layer_get_bounds(window_layer);
 	
   // Create the TextLayer with specific bounds
-  s_time_layer_h = text_layer_create(GRect((bounds.size.w/2)-47+10, (bounds.size.h/2)-84, 47, 1216));
-  s_time_layer_m = text_layer_create(GRect((bounds.size.w/2)+10, (bounds.size.h/2)-84, 27, 1488));
+  s_time_layer_h = text_layer_create(GRect((bounds.size.w/2)-49+xOffset, (bounds.size.h/2)-84, 47, 1216));
+  s_time_layer_m = text_layer_create(GRect((bounds.size.w/2)+xOffset, (bounds.size.h/2)-84, 27, 1488));
 
   // Improve the layout to be more like a watchface
-  text_layer_set_background_color(s_time_layer_h, GColorClear);
-  text_layer_set_text_color(s_time_layer_h, COLORFR);
-  text_layer_set_text(s_time_layer_h, "21\n22\n23\n00\n01\n02\n03\n04\n05\n06\n07\n08\n09\n10\n11\n12\n13\n14\n15\n16\n17\n18\n19\n20\n21\n22\n23\n00\n01\n02\n03\n04");
-  text_layer_set_text_alignment(s_time_layer_h, GTextAlignmentCenter);
+  text_layer_set_background_color(s_time_layer_h, COLORHRBG);
+  text_layer_set_text_color(s_time_layer_h, COLORHRFR);
+  if (twelveHour == 1) {
+		text_layer_set_text(s_time_layer_h, "09\n10\n11\n12\n01\n02\n03\n04\n05\n06\n07\n08\n09\n10\n11\n12\n01\n02\n03\n04\n05\n06\n07\n08\n09\n10\n11\n12\n01\n02\n03\n04");
+	} else {
+		text_layer_set_text(s_time_layer_h, "21\n22\n23\n00\n01\n02\n03\n04\n05\n06\n07\n08\n09\n10\n11\n12\n13\n14\n15\n16\n17\n18\n19\n20\n21\n22\n23\n00\n01\n02\n03\n04");
+	}
+	text_layer_set_text_alignment(s_time_layer_h, GTextAlignmentCenter);
 
- text_layer_set_background_color(s_time_layer_m, GColorClear);
-  text_layer_set_text_color(s_time_layer_m, COLORFR);
-  text_layer_set_text(s_time_layer_m, "55\n56\n57\n58\n59\n00\n01\n02\n03\n04\n05\n06\n07\n08\n09\n10\n11\n12\n13\n14\n15\n16\n17\n18\n19\n20\n21\n22\n23\n24\n25\n26\n27\n28\n29\n30\n31\n32\n33\n34\n35\n36\n37\n38\n39\n40\n41\n42\n43\n44\n45\n46\n47\n48\n49\n50\n51\n52\n53\n54\n55\n56\n57\n58\n59\n00\n01\n02\n03\n04");
-  text_layer_set_text_alignment(s_time_layer_m, GTextAlignmentCenter);
+	text_layer_set_background_color(s_time_layer_m, COLORMNBG);
+  	text_layer_set_text_color(s_time_layer_m, COLORMNFR);
+	text_layer_set_text(s_time_layer_m, "55\n56\n57\n58\n59\n00\n01\n02\n03\n04\n05\n06\n07\n08\n09\n10\n11\n12\n13\n14\n15\n16\n17\n18\n19\n20\n21\n22\n23\n24\n25\n26\n27\n28\n29\n30\n31\n32\n33\n34\n35\n36\n37\n38\n39\n40\n41\n42\n43\n44\n45\n46\n47\n48\n49\n50\n51\n52\n53\n54\n55\n56\n57\n58\n59\n00\n01\n02\n03\n04");
+  	text_layer_set_text_alignment(s_time_layer_m, GTextAlignmentCenter);
 	
 
 	// add fonts
@@ -284,9 +329,12 @@ static void main_window_load(Window *window) {
   	layer_set_update_proc(s_canvas, layer_update_proc);
   	layer_add_child(window_layer, s_canvas);
 	
+	// ************************************************
+	// ** Drop shadow
+	// ************************************************
 
  // Create GBitmap
-  s_background_bitmap = gbitmap_create_with_resource(RESOURCE_ID_ARROWS);
+  s_background_bitmap = gbitmap_create_with_resource(RESOURCE_ID_DROP);
 
   // Create BitmapLayer to display the GBitmap
   s_background_layer = bitmap_layer_create(
@@ -305,6 +353,20 @@ static void main_window_load(Window *window) {
 
 	// Add to Window
 	layer_add_child(window_get_root_layer(window), s_canvas_line);
+
+
+	// ************************************************
+	// ** Arrows
+	// ************************************************
+	
+	s_arrows = layer_create(GRect(((bounds.size.w/2)-55)+pmSpace, ((bounds.size.h/2)-7), 110+(pmSpace*2), 14));
+  	layer_set_update_proc(s_arrows, s_arrows_update_proc);
+  //---add the layer to the Window layer---
+  	layer_add_child(window_layer, s_arrows);
+	
+	
+	
+
 }
 	
 static void main_window_unload(Window *window) {
@@ -323,10 +385,9 @@ static void main_window_unload(Window *window) {
 	
 	// Destroy Canvas line
 	layer_destroy(s_canvas_line);
-
-  // Remove defines
- #undef COLORBG;
- #undef COLORFR;
+	
+	//destroy arrows
+	layer_destroy(s_arrows);
 	
 }
 
