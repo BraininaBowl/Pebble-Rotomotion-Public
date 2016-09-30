79ecc1bcc9853e8ebfd953fd4f91b8907a42a96d
Quistnix <fransvb@gmail.com>
2016-09-30 11:18:44 +0200

    w

--- src/c/main.c
+++ src/c/main.c
@@ -294,11 +294,11 @@ yToGet = yToSet + (colHalf/(yToUse));
 			  if (x < rowHalf) {
 				  // left half: Work from right to left
 				  xToUse = rowHalf - x;
-				  xToGet = xToUse + (y-colHalf);
+				  xToGet = xToUse + ((y-colHalf)/2);
 			  } else {
 				  // right half: Work from left to right
 				  xToUse = x;
-				  xToGet = xToUse - (y-colHalf);
+				  xToGet = xToUse - ((y-colHalf)/2);
 			  }
 			  // is the target pixel inside the area?
 			  if (xToGet < 0 || xToGet >= rowFull || yToGet < 0 || yToGet > colFull ){
