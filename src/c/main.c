880b5cd83205f441a8980ffb013ca6feb5c1057b
Quistnix <fransvb@gmail.com>
2016-10-03 15:08:43 +0200

    w

--- src/c/main.c
+++ src/c/main.c
@@ -294,15 +294,15 @@ yToGet = yToSet + (colHalf/(yToUse));
 		     GColor color3 = get_bitmap_pixel_color(fb, fb_format, y, xToUse+2);
 		     GColor color4 = get_bitmap_pixel_color(fb, fb_format, y, xToUse+4);
 		     GColor color5 = get_bitmap_pixel_color(fb, fb_format, y+1, xToUse);
-		     GColor color6 = get_bitmap_pixel_color(fb, fb_format, y+2, xToUse);
-		     GColor color7 = get_bitmap_pixel_color(fb, fb_format, y+3, xToUse);
-		     GColor color8 = get_bitmap_pixel_color(fb, fb_format, y+4, xToUse);	
+		     GColor color6 = get_bitmap_pixel_color(fb, fb_format, y+1, xToUse + 1);
+		     GColor color7 = get_bitmap_pixel_color(fb, fb_format, y+2, xToUse);
+		     
 		     	
 		     	  
 		     GColor colorToSet = GColorFromRGB(
-		     (color1.r + color2.r + color3.r + color4.r + color5.r + color6.r + color7.r + color8.r)*85/8, 
-		     (color1.g + color2.g + color3.g + color4.g + color5.g + color6.g + color7.g + color8.g)*85/8,		     
-		     (color1.b + color2.b + color3.b + color4.b + color5.b + color6.b + color7.b + color8.b)*85/8);
+		     (color1.r + color2.r + color3.r + color4.r + color5.r + color6.r + color7.r)*85/7, 
+		     (color1.g + color2.g + color3.g + color4.g + color5.g + color6.g + color7.g)*85/7,		     
+		     (color1.b + color2.b + color3.b + color4.b + color5.b + color6.b + color7.b)*85/7);
 		      
 			  
 		     
