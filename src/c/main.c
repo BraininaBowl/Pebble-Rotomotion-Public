c48f8edad72833f60e526b13ea9f5411bc8b68fe
Quistnix <fransvb@gmail.com>
2016-10-03 09:23:00 +0200

    w

--- src/c/main.c
+++ src/c/main.c
@@ -298,9 +298,9 @@ yToGet = yToSet + (colHalf/(yToUse));
 		     	
 		     	  
 		     GColor colorToSet = GColorFromRGB(
-		     (color1.r + color2.r + color3.r + color4.r + color5.r + color6.r)*85/6, 
-		     (color1.g + color2.g + color3.g + color4.g + color5.g + color6.g)*85/6,		     
-		     (color1.b + color2.b + color3.b + color4.b + color5.b + color6.b)*85/6);  
+		     (color1.r + color2.r + color3.r + color4.r + color5.r + color6.r - 6)*85/6, 
+		     (color1.g + color2.g + color3.g + color4.g + color5.g + color6.g - 6)*85/6,		     
+		     (color1.b + color2.b + color3.b + color4.b + color5.b + color6.b - 6)*85/6);  
 			    
 			    
 			    
