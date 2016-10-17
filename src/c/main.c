959f8446f3831fd40cfd007487c04411f4595999
Quistnix <fransvb@gmail.com>
2016-10-17 10:24:14 +0200

    w

--- src/c/main.c
+++ src/c/main.c
@@ -174,9 +174,13 @@ if (settings.shaderMode == 1) {
 						  (colorToSet.g * 3 + settings.BackgroundColor.g)*85/4, 
 						  (colorToSet.b * 3 + settings.BackgroundColor.b)*85/4);
 					}				
-				}  	 				
-		 		
-			  
+				}  	
+				
+				// fix top edge
+				if( y = 0) {
+						colorToSet = settings.BackgroundColor;
+					} 				
+		 		  
 		 		// Now we set the pixel to the right color
 		 		set_bitmap_pixel_color(fb, fb_format, yToSet, xToUse, colorToSet);
 			  }
