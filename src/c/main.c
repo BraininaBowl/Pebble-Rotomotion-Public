84ff4e98afc57420218f0db4e93ea967ddcfd451
Quistnix <fransvb@gmail.com>
2016-10-18 10:10:31 +0200

    w

--- src/c/main.c
+++ src/c/main.c
@@ -191,7 +191,8 @@ if (settings.shaderMode == 1) {
 	
 		// zwarte balk	
 			#if defined(PBL_COLOR)
-	  	if (yToSet < 14 || yToSet > (colFull - 13)){
+	  //	if (yToSet < 14 || yToSet > (colFull - 13)){
+	  if (yToSet < 5 || yToSet > (colFull - 4)){
 			for(int x = 0; x < rowFull; x++) {
 				// Now we set the pixel to the right color
 		 		set_bitmap_pixel_color(fb, fb_format, yToSet, x, settings.BackgroundColor);
